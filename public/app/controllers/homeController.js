45842c5467a587505c2c39a20b534bd5d065fcc8
Jake <jake@Jakes-Air.home>
2017-10-21 15:23:42 -0400

    Add loading notification when loading location and directions

--- public/app/controllers/homeController.js
+++ public/app/controllers/homeController.js
@@ -8,20 +8,24 @@ function HomeController(TravelSmartFactory, GoogleMapsFactory, GeoLocationFactor
 	var vm = this;
 	vm.events = [];
 
-	//activate();
+	activate();
 
 	//////////////
 
 	vm.getDirections = function(){
+		vm.loading = true;
 		GoogleMapsFactory.getTransitDirections(vm.currentLocation, vm.destination).then(function(res){
 			vm.directions = res.routes[0].legs[0].steps;
+			vm.loading = false;
 		});
 	}
 
 	vm.getLocation = function(){
+		vm.loading = true;
 		GeoLocationFactory.getLocation(function(position){
 			GoogleMapsFactory.reverseGeocode(position.coords.latitude, position.coords.longitude).then(function(res){
 				if(res.results.length > 0)vm.currentLocation = res.results[0].formatted_address;
+				vm.loading = false;
 			})
 		})
 	}
